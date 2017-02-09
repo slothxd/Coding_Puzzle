27a4408fe4287a601326b0dd1cc1315a341d180d
slothxd <jasonxdd@gmail.com>
2017-02-09 01:59:33 -0800

    some puzzle
    took me 30 mins on and off....

---
+++ fizzbuzz.java
@@ -0,0 +1,19 @@
+package fizzbuzz;
+
+public class fizzbuzz{
+
+public static void main(String []arg){
+	for(int i=1; i < 101; i++){
+		if (i % 3 == 0 & i % 5 == 0){
+			System.out.println("fizzbuzz");
+		}
+		else if(i % 3 == 0) { 
+			System.out.println("fizz");
+		}
+		else if (i% 5 == 0){
+			System.out.println("buzz");
+		}
+		else System.out.println(i);
+	}
+}
+}
